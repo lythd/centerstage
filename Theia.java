d3007e7ee6d4e46c177068006f2f8b36283c8a5d
ly <mikestyle05@gmail.com>
2024-01-21 16:24:40 -0500

    Create Theia.java

---
+++ Theia.java
@@ -0,0 +1,48 @@
+package org.firstinspires.ftc.teamcode.auto;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.vision.VisionPortal;
+
+@Autonomous
+public class Theia extends LinearOpMode {
+
+    private CSVisionProcessor visionProcessor;
+    private VisionPortal visionPortal;
+
+    @Override
+    public void runOpMode() {
+
+        visionProcessor = new CSVisionProcessor();
+        visionPortal = VisionPortal.easyCreateWithDefaults(hardwareMap.get(WebcamName.class, "Webcam 1"), visionProcessor);
+
+        CenterStageProcessor.StartingPosition startingPos = CenterStageProcessor.StartingPosition.NONE;
+
+        //waitForStart();
+
+        while(!this.isStarted() && !this.isStopRequested()) {
+            startingPos = visionProcessor.getStartingPosition();
+            telemetry.addData("Identified", visionProcessor.getStartingPosition());
+            telemetry.update();
+        }
+
+        visionPortal.stopStreaming();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+            //use the value of startingPos to determine your location
+
+        }
+
+    }
+
+
+
+
+
+
+}
